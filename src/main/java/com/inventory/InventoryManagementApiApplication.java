68fbc2b05853efb2f1bb343d2de019af3cc8a1a1
Sheldon <sheldonkhlim@gmail.com>
2024-09-25 18:57:17 +0800

    Initial commit, project setup

---
+++ src/main/java/com/inventory/InventoryManagementApiApplication.java
@@ -0,0 +1,12 @@
+package com.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryManagementApiApplication.class, args);
+    }
+}
